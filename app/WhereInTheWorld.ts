dc1d70703dfe1219ed267c40ac27ffc1846a11c9
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-04-26 17:10:13 +1200

    Main class.
    Intended to have code for displaying GeoJSON output

---
+++ app/WhereInTheWorld.ts
@@ -0,0 +1,12 @@
+/**
+ * Class for the "Where in the World" Program
+ */
+import {ParseInput} from "./ParseInput";
+
+export class WhereInTheWorld {
+
+    public start() {
+        new ParseInput().start()
+    }
+}
+    
