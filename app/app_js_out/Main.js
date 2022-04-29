2fc07d58e59bdf47c095c6e671693d49c15f5da2
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-04-29 17:20:31 +1200

    admin changes

--- app/app_js_out/Main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Start the "Where in the world" program
-Object.defineProperty(exports, "__esModule", { value: true });
-const ParseInput_1 = require("./ParseInput");
-new ParseInput_1.ParseInput().start();
-//# sourceMappingURL=Main.js.map
