3cc9a0d04e3c09722e4634cd91e226da04d76ad0
HugoPhibbs <hugophibbs1@gmail.com>
2022-05-06 21:21:08 +1200

    changes to main
    now supports file input

--- app/Main.ts
+++ app/Main.ts
@@ -1,8 +1,19 @@
 // Start the "Where in the world" program
 
 import {ParseInput} from './ParseInput'
-import {getInput, writeToOutput} from "./OutputInput";
+import {getInput, writeToOutput, fileToLineArray} from "./OutputInput";
 
-let input = getInput();
-let parsedLines = new ParseInput().parseLines(input);
+const args = process.argv.slice(2)
+console.assert(args.length < 2, "Only one input directory should be entered, otherwise it should be none")
+
+console.log(process.cwd())
+
+let parsedLines;
+if (args.length == 1){
+    parsedLines = new ParseInput().parseLines(fileToLineArray(args[0]))
+}
+else {
+    parsedLines = new ParseInput().parseLines(getInput());
+}
+
 writeToOutput(parsedLines)
