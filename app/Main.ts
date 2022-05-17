1793c7f5a203a924bd3b394ecf0aade3eb45e09e
HugoPhibbs <hugophibbs1@gmail.com>
2022-05-17 16:09:56 +1200

    Changes to main
    refactoring

--- app/Main.ts
+++ app/Main.ts
@@ -1,17 +1,5 @@
 // Start the "Where in the world" program
 
-import {ParseInput} from './ParseInput'
-import {getInput, writeToOutput, fileToLineArray} from "./OutputInput";
+import {getInput, writeToOutput, fileToLineArray, handleArgInput} from "./OutputInput";
 
-const args = process.argv.slice(2)
-console.assert(args.length < 2, "Only one input directory should be entered, otherwise it should be none")
-
-let parsedLines;
-if (args.length == 1){
-    parsedLines = new ParseInput().parseLines(fileToLineArray(args[0]))
-}
-else {
-    parsedLines = new ParseInput().parseLines(getInput());
-}
-
-writeToOutput(parsedLines)
+writeToOutput(handleArgInput(process.argv.slice(2)))
